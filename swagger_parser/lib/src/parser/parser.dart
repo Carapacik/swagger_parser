e7dbb09980e0b00f26ec2b147423d39e89f0c52c
Pablo Sanchez <pablo.sanchez@s3connectedhealth.com>
2022-12-12 13:20:56 +0000

    Removing accidentally commited values still unused

--- swagger_parser/lib/src/parser/parser.dart
+++ swagger_parser/lib/src/parser/parser.dart
@@ -62,8 +62,6 @@ class OpenApiJsonParser {
   static const _schemasVar = 'schemas';
   static const _pathsVar = 'paths';
   static const _allOfVar = 'allOf';
-  static const _oneOfVar = 'oneOf';
-  static const _anyOfVar = 'anyOf';
 
   /// Parses rest clients from "paths" section of json file into universal models
   Iterable<UniversalRestClient> parseRestClients() {
