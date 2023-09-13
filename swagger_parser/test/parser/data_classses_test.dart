093b2a4f864d3e69626480787ad1b22a96ac6d4f
AlexDekhant <sasha.dekhant@gmail.com>
2023-09-13 10:02:35 +0300

    parser tests 2

--- swagger_parser/test/parser/data_classses_test.dart
+++ swagger_parser/test/parser/data_classses_test.dart
@@ -8,8 +8,7 @@ import 'package:test/test.dart';
 void main() {
   group('Empty data class', () {
     test('2.0', () async {
-      final schemaFilePath =
-          p.join('test', 'parser', 'schemas', 'empty_class.2.0.json');
+      final schemaFilePath = p.join('test', 'parser', 'schemas', 'empty_class.2.0.json');
       final configFile = schemaFile(schemaFilePath);
       final schemaContent = configFile!.readAsStringSync();
       final parser = OpenApiParser(schemaContent);
@@ -23,8 +22,7 @@ void main() {
     });
 
     test('3.0', () async {
-      final schemaFilePath =
-          p.join('test', 'parser', 'schemas', 'empty_class.3.0.json');
+      final schemaFilePath = p.join('test', 'parser', 'schemas', 'empty_class.3.0.json');
       final configFile = schemaFile(schemaFilePath);
       final schemaContent = configFile!.readAsStringSync();
       final parser = OpenApiParser(schemaContent);
@@ -38,10 +36,9 @@ void main() {
     });
   });
 
-  group('Types check', () {
+  group('Types check 2.0', () {
     test('basic types check', () async {
-      final schemaFilePath =
-          p.join('test', 'parser', 'schemas', 'basic_types_class.2.0.json');
+      final schemaFilePath = p.join('test', 'parser', 'schemas', 'basic_types_class.2.0.json');
       final configFile = schemaFile(schemaFilePath);
       final schemaContent = configFile!.readAsStringSync();
       final parser = OpenApiParser(schemaContent);
@@ -120,9 +117,135 @@ void main() {
       );
       expect(actualDataClass, expectedDataClass);
     });
-    test('composite types check', () async {
-      final schemaFilePath =
-          p.join('test', 'parser', 'schemas', 'reference_types_class.2.0.json');
+
+    test('basic types check 3.0', () async {
+      final schemaFilePath = p.join('test', 'parser', 'schemas', 'basic_types_class.3.0.json');
+      final configFile = schemaFile(schemaFilePath);
+      final schemaContent = configFile!.readAsStringSync();
+      final parser = OpenApiParser(schemaContent);
+      final actualDataClass = parser.parseDataClasses().first;
+      const expectedDataClass = UniversalComponentClass(
+        name: 'ClassName',
+        imports: {},
+        parameters: [
+          UniversalType(
+            type: 'integer',
+            name: 'integer1',
+            jsonKey: 'integer1',
+          ),
+          UniversalType(
+            type: 'number',
+            format: 'float',
+            name: 'float1',
+            jsonKey: 'float1',
+          ),
+          UniversalType(
+            type: 'number',
+            format: 'double',
+            name: 'double1',
+            jsonKey: 'double1',
+          ),
+          UniversalType(
+            type: 'number',
+            format: 'string',
+            name: 'string1',
+            jsonKey: 'string1',
+          ),
+          UniversalType(
+            type: 'number',
+            name: 'number1',
+            jsonKey: 'number1',
+          ),
+          UniversalType(
+            type: 'string',
+            name: 'string2',
+            format: 'binary',
+            jsonKey: 'string2',
+          ),
+          UniversalType(
+            type: 'string',
+            name: 'string3',
+            format: 'date',
+            jsonKey: 'string3',
+          ),
+          UniversalType(
+            type: 'string',
+            name: 'string4',
+            format: 'datetime',
+            jsonKey: 'string4',
+          ),
+          UniversalType(
+            type: 'string',
+            name: 'string5',
+            jsonKey: 'string5',
+          ),
+          UniversalType(
+            type: 'file',
+            name: 'file1',
+            jsonKey: 'file1',
+          ),
+          UniversalType(
+            type: 'boolean',
+            name: 'bool1',
+            jsonKey: 'bool1',
+          ),
+          UniversalType(
+            type: 'object',
+            name: 'object1',
+            jsonKey: 'object1',
+          ),
+        ],
+      );
+      expect(actualDataClass, expectedDataClass);
+    });
+
+    test('composite types check 3.0', () async {
+      final schemaFilePath = p.join('test', 'parser', 'schemas', 'reference_types_class.2.0.json');
+      final configFile = schemaFile(schemaFilePath);
+      final schemaContent = configFile!.readAsStringSync();
+      final parser = OpenApiParser(schemaContent);
+      final dataClasses = parser.parseDataClasses().toList();
+      const expectedDataClasses = [
+        UniversalComponentClass(
+          name: 'ClassName',
+          imports: {'AnotherClass'},
+          parameters: [
+            UniversalType(
+              type: 'integer',
+              name: 'id',
+              jsonKey: 'id',
+            ),
+            UniversalType(
+              type: 'AnotherClass',
+              name: 'another',
+              jsonKey: 'another',
+            ),
+          ],
+        ),
+        UniversalComponentClass(
+          name: 'AnotherClass',
+          imports: {},
+          parameters: [
+            UniversalType(
+              type: 'integer',
+              name: 'id',
+              jsonKey: 'id',
+            ),
+            UniversalType(
+              type: 'string',
+              name: 'name',
+              jsonKey: 'name',
+            ),
+          ],
+        ),
+      ];
+      for (var i = 0; i < dataClasses.length; i++) {
+        expect(dataClasses[i], expectedDataClasses[i]);
+      }
+    });
+
+    test('composite types check 3.0', () async {
+      final schemaFilePath = p.join('test', 'parser', 'schemas', 'reference_types_class.3.0.json');
       final configFile = schemaFile(schemaFilePath);
       final schemaContent = configFile!.readAsStringSync();
       final parser = OpenApiParser(schemaContent);
