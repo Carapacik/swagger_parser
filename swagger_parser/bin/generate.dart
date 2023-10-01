b49656ed665a708e190b881b21da77f1c269eb4f
StarProxima <maks.prozorov1@yandex.ru>
2023-10-01 19:38:16 +0300

    Merge YamlConfigManager with YamlConfig

--- swagger_parser/bin/generate.dart
+++ swagger_parser/bin/generate.dart
@@ -1,4 +1,4 @@
-import 'package:swagger_parser/src/config/yaml_config_manager.dart';
+import 'package:swagger_parser/src/config/yaml_config.dart';
 import 'package:swagger_parser/src/utils/utils.dart';
 import 'package:swagger_parser/swagger_parser.dart';
 
@@ -7,7 +7,7 @@ Future<void> main(List<String> arguments) async {
   introMessage();
   try {
     /// Run generate from YAML config
-    final configs = YamlConfigManager.parseConfigsFromYamlFile(arguments);
+    final configs = YamlConfig.parseConfigsFromYamlFile(arguments);
 
     generateMessage();
     for (final config in configs) {
