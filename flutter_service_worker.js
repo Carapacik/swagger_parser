4d87bd18fb042f2161a12a0fbf079e7ede62b5d3
Carapacik <Carapacik@users.noreply.github.com>
2024-01-11 09:36:34 +0000

    deploy: 8a7bd194478ba990161145cd7645ebfc0fd9f74c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
 "manifest.json": "dbdbc9356e82e8479572ab3c7c565cd2",
-"index.html": "e6036ca5599b7dedbc34163cc976fe89",
-"/": "e6036ca5599b7dedbc34163cc976fe89",
+"index.html": "097287c40eff2f4ac14dd4a9eb374fc2",
+"/": "097287c40eff2f4ac14dd4a9eb374fc2",
 "assets/AssetManifest.bin": "0b0a3415aad49b6e9bf965ff578614f9",
 "assets/fonts/MaterialIcons-Regular.otf": "32fce58e2acb9c420eab0fe7b828b761",
 "assets/AssetManifest.bin.json": "a1fee2517bf598633e2f67fcf3e26c94",
