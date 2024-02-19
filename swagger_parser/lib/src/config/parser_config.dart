dcf94ddea29b60dd6f02ed5c96bfa12509501042
Roman Laptev <carapacik@gmail.com>
2024-02-19 16:38:34 +0300

    Parser config

---
+++ swagger_parser/lib/src/config/parser_config.dart
@@ -0,0 +1,44 @@
+///
+class ParserConfig {
+  ///
+  const ParserConfig(
+    this.content, {
+    required this.isJson,
+    this.defaultContentType = 'application/json',
+    this.pathMethodName = false,
+    this.requiredByDefault = true,
+    this.mergeClients = false,
+    this.skippedParameters = const [],
+  });
+
+  /// Specification file content
+  final Map<String, dynamic> content;
+
+  /// Set `true` if your specification is json.
+  /// Set `false` if yaml.
+  final bool isJson;
+
+  /// DART ONLY
+  /// Default content type for all requests and responses.
+  ///
+  /// If the content type does not match the default, generates:
+  /// @Headers(<String, String>{'Content-Type': 'PARSED CONTENT TYPE'})
+  final String defaultContentType;
+
+  /// DART ONLY
+  /// It is used if the value does not have the annotations `required` and `nullable`.
+  /// If the value is `true`, then value be `required`.
+  /// If the value is `false`, then `nullable`.
+  final bool requiredByDefault;
+
+  /// If `true`, use the endpoint path for the method name.
+  /// if `false`, use `operationId`.
+  final bool pathMethodName;
+
+  /// DART ONLY
+  /// TODO
+  final bool mergeClients;
+
+  /// List of parameter names that should skip during parsing
+  final List<String> skippedParameters;
+}
