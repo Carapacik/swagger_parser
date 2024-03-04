f68dcc37823585415bb630715d717fbf5b882685
Moshe Dicker <dickermoshe@gmail.com>
2024-03-04 09:23:29 -0500

    add backslash to jsonKey protection

--- swagger_parser/lib/src/parser/utils/type_utils.dart
+++ swagger_parser/lib/src/parser/utils/type_utils.dart
@@ -216,5 +216,5 @@ final _nameRegExp = RegExp(r'^[a-zA-Z_][a-zA-Z\d_]*$');
 
 /// Protect JsonKeys from incorrect symbols, keywords, etc.
 String? protectJsonKey(String? name) {
-  return name?.replaceAll(r'$', r'\$');
+  return name?.replaceAll(r'\', r'\\').replaceAll(r'$', r'\$');
 }
