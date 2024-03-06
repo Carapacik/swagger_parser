446648736f663b6c5b45c07cdc7f5c3fe161a7a3
dfdgsdfg <dfdgsdfg@gmail.com>
2024-03-06 16:38:38 +0900

    #211

---
+++ swagger_parser/test/e2e/tests/multipart_request_properties_required_by_default_false/expected_files/export.dart
@@ -0,0 +1,11 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, unused_import
+
+// Clients
+export 'clients/client_client.dart';
+// Data classes
+export 'models/object0.dart';
+export 'models/object1.dart';
+// Root client
+export 'rest_client.dart';
