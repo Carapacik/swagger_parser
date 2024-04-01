6b55d22faf217a9e580b0f17aa022e95c10bcca5
Carapacik <Carapacik@users.noreply.github.com>
2024-04-01 07:35:39 +0000

    deploy: bb9eae14de7768631419df301a6868494c4c2e75

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"index.html": "b0edb206885d8144e545ff0911179b90",
-"/": "b0edb206885d8144e545ff0911179b90",
+const RESOURCES = {"index.html": "d5e18fa11edbbc945d06482f0ec46be6",
+"/": "d5e18fa11edbbc945d06482f0ec46be6",
 "canvaskit/canvaskit.js": "c86fbd9e7b17accae76e5ad116583dc4",
 "canvaskit/skwasm.js": "445e9e400085faead4493be2224d95aa",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
