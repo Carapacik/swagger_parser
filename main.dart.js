e6276f9e8c43a63688f25f9103420bde3a0cf8c7
Carapacik <Carapacik@users.noreply.github.com>
2025-07-29 14:29:01 +0000

    deploy: a3f31a879c864a999cf02a5316c85d8ca524d35a

--- main.dart.js
+++ main.dart.js
@@ -25467,7 +25467,7 @@ $S:2}
 A.alW.prototype={
 $1(a){var s=A.e8().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/39d6d6e699e51b2874210e14cddf1a22fb9524b2/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/ef0cd000916d64fa0c5d09cc809fa7ad244a5767/":s)+a},
 $S:22}
 A.Vu.prototype={
 f1(a,b){var s,r=this.a
