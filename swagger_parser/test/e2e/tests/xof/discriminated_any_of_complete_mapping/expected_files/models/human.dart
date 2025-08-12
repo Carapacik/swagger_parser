bb5b721ffc492ecd09ced9e7ffa625da3efbdc16
Peter Trost <p.trost93@gmail.com>
2025-08-12 10:04:28 +0200

    feat: add handling of undiscriminated unions

---
+++ swagger_parser/test/e2e/tests/xof/discriminated_any_of_complete_mapping/expected_files/models/human.dart
@@ -0,0 +1,23 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, unused_import
+
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'family_members_union.dart';
+import 'human_type.dart';
+
+part 'human.freezed.dart';
+part 'human.g.dart';
+
+@Freezed()
+class Human with _$Human {
+  const factory Human({
+    required HumanType type,
+
+    /// The job of the human.
+    required String job,
+  }) = _Human;
+
+  factory Human.fromJson(Map<String, Object?> json) => _$HumanFromJson(json);
+}
