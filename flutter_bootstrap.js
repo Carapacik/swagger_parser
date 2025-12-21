2cdfa6c631acba9ccfee5200183ad962022a1c85
Carapacik <Carapacik@users.noreply.github.com>
2025-12-21 23:11:43 +0000

    deploy: 412cd7cb0b801e08efd9c052d4a274a792eb3874

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -34,10 +34,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d89775053","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
+_flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9bf","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1791100910"
+    serviceWorkerVersion: "2569093819"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "0ce419732ea8325d67cd8b9981039a1c",
-"flutter_bootstrap.js": "2a6f9ee5207955eb2b8eb2d617a96498",
+"flutter_bootstrap.js": "eedef8e370a00865cf336c035235b982",
 "version.json": "a95d5bfbbaff6208c6096221013322b2",
-"main.dart.js": "5abb1de8543853667f8d915d1125144b"};
+"main.dart.js": "2dd57c7b6f3e7be53b970708c83db437"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
