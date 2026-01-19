6cfccaed133cbe1ac895ea64474536a6e09f1438
Carapacik <Carapacik@users.noreply.github.com>
2026-01-19 06:10:00 +0000

    deploy: c49709c6a3c45fb1850da1e7bc1419b61315827a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1673221403"
+    serviceWorkerVersion: "151659664"
   }
 });
