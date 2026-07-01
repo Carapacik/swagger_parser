b3e455782925913da5da48deac034346c5cd992f
Tomas Verhelst <tomasverhelst@gmail.com>
2026-07-01 15:37:15 +0200

    enum value tests

---
+++ swagger_parser/test/e2e/tests/enum_types_list_mappable/expected_files/export.dart
@@ -0,0 +1,14 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, unused_import, invalid_annotation_target, unnecessary_import
+
+// Clients
+export 'clients/api_client.dart';
+// Data classes
+export 'models/nullable_enum_in_object.dart';
+export 'models/enum_class.dart';
+export 'models/enum_class_dynamic.dart';
+export 'models/fruits.dart';
+export 'models/credential_types.dart';
+// Root client
+export 'rest_client.dart';
